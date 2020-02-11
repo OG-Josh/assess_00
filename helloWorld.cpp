0c2c37431dc70d59d4a3ab80472253aafcf01f4f
Josh <36059029+PGz-Josh@users.noreply.github.com>
2020-02-11 12:48:22 -0800

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,57 @@
+//**********************************************************************
+//					Program is written by Joshua Yang
+//					Student ID is 2355145
+//					Professor of course is Rene German
+//					Date is 2/11/2020
+//					Assignment objective is "Hello World"
+//
+//
+//
+//
+//
+//
+//**********************************************************************
+
+
+//Libraries required/used for this
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<cstdio>
+#include<iomanip>
+#include<ctime>
+#include<cctype>
+#include<cstdlib>
+
+//Include Predefined Identifiers
+using namespace std;
+
+//Function Declarations
+
+
+int main(int argc, char* argv[])
+{
+	//Data Declarations
+	cout << "Hello world!" << endl;
+
+
+
+
+
+	//Return data
+	return 0;
+}
+//References:
+//system("pause");
+
+//Algorithm Setup			|			Corresponding C++ Statements
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
